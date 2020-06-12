2bccf27623cb65f38ec6395b9c85c5af3b504257
isys35 <isys35@mail.ru>
2020-06-12 11:19:24 +0300

    init

--- textil_parser.py
+++ textil_parser.py
@@ -1,12 +1,13 @@
 from parsing_base import Parser
 from bs4 import BeautifulSoup
-
+import os
 
 class TextilParser(Parser):
     MAIN_PAGE = 'https://teks-o-park.com'
 
     def __init__(self):
         super().__init__()
+        self.image_catalog = 'img'
 
     def update_data(self):
         resp = self.request.get('https://teks-o-park.com/shop/')
@@ -25,10 +26,24 @@ class TextilParser(Parser):
             materials_blocks = soup.select('.woocommerce-LoopProduct-link')
             for material_block in materials_blocks:
                 materials_urls.append(material_block['href'])
-        splited_materials_urls = self.split_list(materials_urls, 10)
-        for url_list in splited_materials_urls:
-            resps_materials = self.requests.get(url_list)
-            print("OK")
+        materials_data = []
+        self.init_image_catalog()
+        for url in materials_urls:
+            resp = self.request.get(url)
+            soup = BeautifulSoup(resp.text, 'lxml')
+            img_url = soup.find('meta', attrs={'property': 'og:image'})['content']
+            image_name = img_url.split('/')[-1]
+            self.save_image(img_url, f"{self.image_catalog}/{image_name}")
+
+    def init_image_catalog(self):
+        try:
+            os.listdir(self.image_catalog)
+        except FileNotFoundError:
+            os.mkdir(self.image_catalog)
+        for the_file in os.listdir(self.image_catalog):
+            file_path = os.path.join(self.image_catalog, the_file)
+            os.unlink(file_path)
+
 
 
 
